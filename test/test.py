653f5f77f8cc4a7838edc855b54ac0b2127bc5db
MishaShabal <221675@edu.fa.ru>
2023-05-27 14:09:44 +0300

    debbugging process

--- test/test.py
+++ test/test.py
@@ -1,18 +1,21 @@
 import io
 import unittest
-
 import PIL.Image
 import requests
+# import services.server_221675
 from requests import request
 
 
 class MyTestCase(unittest.TestCase):
+    # def setUp(self):
+        # app.run()
+
     def test_home(self):
         response = requests.request('GET', 'http://localhost:1675/')
         sample = response.content.decode()
-        self.assertEqual(sample, 'Home page')
+        self.assertEqual(sample, 'Home page')  # add assertion here
 
-    def test_imagenet_classify(self):
+    def test_classify_imagenet(self):
         img = PIL.Image.open('../data/dog.jpg')
         buffer = io.BytesIO()
         img.save(buffer, format='JPEG')
@@ -27,19 +30,22 @@ class MyTestCase(unittest.TestCase):
 
         self.assertIn(expected, out)
 
-     def test_binary_classify(self):
-            img = PIL.Image.open('../data/cat.jpg')
-            buffer = io.BytesIO()
-            img.save(buffer, format='JPEG')
 
-            with buffer as buf:
-                buffer.seek(0)
-                response = request('POST', 'http://localhost:1675/classify/binary', data=buf)
+    def test_classify_binary(self):
+        img = PIL.Image.open('../data/cat.jpg')
+        buffer = io.BytesIO()
+        img.save(buffer, format='JPEG')
 
-            out = response.content.decode('utf-8')
-            print(out)
-            expected = 'Cat'
-            self.assertEqual(expected, out)
+        with buffer as buf:
+            buffer.seek(0)
+            response = request('POST', 'http://localhost:1675/classify/binary', data=buf)
+
+        out = response.content.decode('utf-8')
+        print(out)
+        expected = 'Cat'
+
+        self.assertEqual(expected, out)
+
 
 
 if __name__ == '__main__':
