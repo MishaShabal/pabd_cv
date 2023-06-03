a39e5a8e9d73c9168b30096592e797f5ec97a7b7
MishaShabal <221675@edu.fa.ru>
2023-06-03 15:13:34 +0300

    evalute evalute.py

---
+++ src/evalute.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import click
+
+# @click.command()
+# @click.option('-d', '--data_dir',
+#               default='pabd_cv/data/raw/pinterest')
+# @click.option('-m', '--model_dir',
+#               default='pabd_cv/data/raw/pinterest')
+# @click.option('-d', '--data_dir',
+#               default='pabd_cv/data/raw/pinterest')
+@click.command()
+@click.option('-d', '--data_dir',
+              default='/home/misha/PycharmProjects/pabd_cv/data/raw/pinterest')
+@click.option('-m', '--model_dir',
+              default='/home/misha/PycharmProjects/pabd_cv/data/raw/pinterest')
+@click.option('-r', '--report_path',
+              default='/home/misha/PycharmProjects/pabd_cv/output/report.txt')
+def evaluate(data_dir, model_dir, report_path):
+    test_df = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        image_size=(180, 180))
+
+    model = tf.keras.models.load_model(model_dir)
+    metrics = [
+        tf.metrics.BinaryAccuracy(),
+        tf.metrics.Precision(),
+        tf.metrics.Recall()
+    ]
+    model.compile(metrics=metrics)
+
+    result = model.evaluate(test_df)
+    loss, acc, precision, recall = result
+    output = [
+        f'test data len: {len(test_df)}\n'
+        f'Accuracy: {acc:.3f}, Precision {precision:.3f}, Recall {recall:.3f}, loss: {loss:.3f}'
+    ]
+
+    with open(report_path, 'w', encoding='utf-8') as f:
+        f.writelines(output)
+
