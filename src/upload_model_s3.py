d70997c237081a31f1d9b2898d3f760410650904
MishaShabal <221675@edu.fa.ru>
2023-06-03 16:22:17 +0300

    fixed upload_model_s3.py

--- src/upload_model_s3.py
+++ src/upload_model_s3.py
@@ -1,18 +1,19 @@
 import boto3
 import dotenv
 
-config = dotenv.dotenv_values('.env')
+config = dotenv.dotenv_values('/home/misha/PycharmProjects/pabd_cv/.env')
+# config = dotenv.dotenv_values('.env')
 ACCESS_KEY = config['ACCESS_KEY']
 SECRET_KEY = config['SECRET_KEY']
 
+model_path = '/home/misha/PycharmProjects/pabd_cv/models/model.zip'
 # model_path = 'models/my_model.zip'
-model_path = '/home/misha/PycharmProjects/pabd_cv/models/my_model.zip'
 
-client = boto3.clent(
+client = boto3.client(
     's3',
-    endpoint_url = 'https://storage.yandex.cloud.net',
-    aws_access_key_id = ACCESS_KEY,
-    aws_secret_access_key = SECRET_KEY
+    endpoint_url='https://storage.yandexcloud.net',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
 )
 
 
