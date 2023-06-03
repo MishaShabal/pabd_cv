6b006e798f853296e257f36821942aaf67398dde
MishaShabal <221675@edu.fa.ru>
2023-06-03 15:22:54 +0300

    fixed evalute.py

--- src/evalute.py
+++ src/evalute.py
@@ -1,6 +1,14 @@
 import tensorflow as tf
 import click
 
+
+@click.command()
+@click.option('-d', '--data_dir',
+              default='/home/misha/PycharmProjects/pabd_cv/data/raw/pinterest')
+@click.option('-m', '--model_dir',
+              default='/home/misha/PycharmProjects/pabd_cv/models/colab_model')
+@click.option('-r', '--report_path',
+              default='/home/misha/PycharmProjects/pabd_cv/output/report.txt')
 # @click.command()
 # @click.option('-d', '--data_dir',
 #               default='pabd_cv/data/raw/pinterest')
@@ -8,18 +16,10 @@ import click
 #               default='pabd_cv/data/raw/pinterest')
 # @click.option('-d', '--data_dir',
 #               default='pabd_cv/data/raw/pinterest')
-@click.command()
-@click.option('-d', '--data_dir',
-              default='/home/misha/PycharmProjects/pabd_cv/data/raw/pinterest')
-@click.option('-m', '--model_dir',
-              default='/home/misha/PycharmProjects/pabd_cv/data/raw/pinterest')
-@click.option('-r', '--report_path',
-              default='/home/misha/PycharmProjects/pabd_cv/output/report.txt')
 def evaluate(data_dir, model_dir, report_path):
     test_df = tf.keras.utils.image_dataset_from_directory(
         data_dir,
         image_size=(180, 180))
-
     model = tf.keras.models.load_model(model_dir)
     metrics = [
         tf.metrics.BinaryAccuracy(),
@@ -34,7 +34,9 @@ def evaluate(data_dir, model_dir, report_path):
         f'test data len: {len(test_df)}\n'
         f'Accuracy: {acc:.3f}, Precision {precision:.3f}, Recall {recall:.3f}, loss: {loss:.3f}'
     ]
-
-    with open(report_path, 'w', encoding='utf-8') as f:
+    with open(report_path, 'w+', encoding='utf-8') as f:
         f.writelines(output)
 
+
+if __name__ == '__main__':
+    evaluate()
